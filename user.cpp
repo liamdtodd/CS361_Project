2d2469c581127e150272915e547153b867b5a2a5
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-10-14 13:15:26 -0700

    UserData constructor, deconstructor, accessor and getter funtions

---
+++ user.cpp
@@ -0,0 +1,64 @@
+#include "user.h"
+#include <fstream>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+UserData::UserData() {
+	name = " ";
+	height = 0;
+	weight = 0;
+	age = 0;
+	fitnessType = " ";
+}
+
+/**************************
+ * ACCESSOR FUNCTIONS FOR THE USERDATA CLASS
+ * ************************/
+string UserData::getName() {
+	return name;
+}
+
+int UserData::getHeight() {
+	return height;
+}
+
+int UserData::getWeight() {
+	return weight;
+}
+
+int UserData::getAge() {
+	return age;
+}
+
+string UserData::getType() {
+	return fitnessType;
+}
+
+
+/************************
+ * SETTER FUNCTIONS FOR THE USERDATA CLASS
+ * *********************/
+void UserData::setName(string nam) {
+	name = nam;
+}
+
+void UserData::setHeight(int h) {
+	height = h;
+}
+
+void UserData::setWeight(int w) {
+	weight = w;
+}
+
+void UserData::setAge(int a) {
+	age = a;
+}
+
+void UserData::setType(int fit) {
+	fitnessType = fit;
+}
+
+//Deconstructor of UserData class
+UserData::~UserData() {}

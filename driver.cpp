1b5a9ab2b7a77a2c8e762558f67a50ace25558a5
Liam Todd <toddl@flip1.engr.oregonstate.edu>
2022-10-16 19:53:19 -0700

    more functionality, more UI

--- driver.cpp
+++ driver.cpp
@@ -6,21 +6,72 @@
 
 using namespace std;
 
-void writeFileData(UserData* user) {
-	string tempPath = "test.txt";
-	fstream file;
-	file.open("test.txt", ios::out);
+//this function print's all of the user's data to the screen
+void print(UserData* user) {
+	cout << "Name: " << user->getName() << endl;
+	cout << "Age: " << user->getAge() << " years" << endl;
+	cout << "Height: " << user->getHeight() << " inches" << endl;
+	cout << "Weight: " << user->getWeight() << " pounds" << endl;
+	cout << "Fitness Goal: " << user->getType() << endl;
+}
 
-	file << user->getName() << endl;
-	file << user->getAge() << endl;
-	file << user->getHeight() << endl;
-	file << user->getWeight() << endl;
-	file << user->getType() << endl;
-	file << user->getfilePath() << endl;
+//this function reads from userdata.txt to find an existing user's data
+void fetchData(UserData* user, string nam) {
+	fstream file;
+	file.open("userdata.txt", ios::in);
+
+	string readstr;
+	int readint;
+
+	while (!file.eof()) {
+		file >> readstr;
+		if (readstr == nam) {
+			user->setName(readstr);
+			
+			file >> readint;
+			user->setAge(readint);
+	
+			file >> readint;
+			user->setHeight(readint);
+			
+			file >> readint;
+			user->setWeight(readint);
+	
+			file >> readstr;
+			user->setType(readstr);
+		}
+	}
+		
+	file.close();
+}
+
+//this function writes all the user's data into the userdata.txt file
+void writeFileData(UserData* user) {
+	fstream file;
+//	file.open("userdata.txt", ios::out);	
+
+	if (file.eof()) {
+		file.open("userdata.txt", ios::out);
+		file << user->getName() << endl;
+		file << user->getAge() << endl;
+		file << user->getHeight() << endl;
+		file << user->getWeight() << endl;
+		file << user->getType() << endl;
+	} 					//first entry of data into userdata.txt
+	else {
+		file.open("userdata.txt", ios::out | ios::app);
+		file << endl;
+		file << user->getName() << endl;
+		file << user->getAge() << endl;
+		file << user->getHeight() << endl;
+		file << user->getWeight() << endl;
+		file << user->getType() << endl;
+	}					//entry of a new user's data into userdata.txt with existing data from other users
 	
 	file.close();
 }
 
+//this function takes in a UserData object and fills it with the corresponding data
 void createUser(UserData* user) {
 	string nam, type, path;
 	int h, w, a;
@@ -40,22 +91,18 @@ void createUser(UserData* user) {
 	cout << endl << "Are you looking to 'bulk' or 'cut' weight? ";
 	cin >> type;
 
-	cout << endl << "Please enter a name for text file where we will store your data: ";
-	cin >> path;
-
 	user->setName(nam);
 	user->setAge(a);
 	user->setHeight(h);
 	user->setWeight(w);
 	user->setType(type);
-	user->setfilePath(path);
 
 	writeFileData(user);
 }
 
 int main(int argc, char** argv) {
 	int ans;
-	string filepath;
+	string nam;
 
 	UserData* user = new UserData();
 
@@ -68,15 +115,21 @@ int main(int argc, char** argv) {
 		if (ans == 1)
 			createUser(user);
 		else {
-			cout << "Please enter a text file, so we can load your user data." << endl;
-			cin >> filepath;
+			cout << "For future use, you can run the software with 'userdata.txt' as an additional arg. Please enter your name: ";
+			cin >> nam;
+			cout << endl;
 		}
 	}
 	
 	else {
-		filepath = argv[1];
-		
+		cout << "Please enter your name: ";
+		cin >> nam;
+		cout << endl;
+		fetchData(user, nam);		
 	}
 
+	print(user);
+
+	delete user;
 	return 0;
 }
